df9e026609ce7e4c5250e08aeed5673a0945787b
Olingo Maxwell <69717524+olingomaxwell1999@users.noreply.github.com>
2023-01-30 18:58:03 +0300

    minor updates

--- components/Amenitiessecond.jsx
+++ components/Amenitiessecond.jsx
@@ -11,14 +11,21 @@ function Amenitiessecond() {
   return (
     <div data-aos='fade-up' className='amenities-second'>
         <div className="amenities-first-heading">
-            <h3>BREATHTAKING VIEWS</h3>
-            <h2>WELL DESIGNED LAYOUTS</h2>
-            <h3>DISTINCTIVE</h3>
+            <h3>ENIGMATIC</h3>
+            <h2>ARCHITECTURE</h2>
+            
         </div>
         <div className="amenities-first-text">
-            <p>Os dolupta ectore vellab ipsae. Ut ut eiur, is expel intis commodignati nobis qui
-ulpa similignis il ipsandebis il inctur, autemperunt hici od quam exerum fugit iumquo
-ilicium reratas eum eum quat quatio voluptat maximil ipsundem volorero.</p>
+            <p>The Reveal, Sets the standard for revolutionary
+architectural innovation and premier living
+experience in the heart of Kilimani,
+Nairobi County.
+The architecture of these beautiful and artfully
+designed units, meticulously set within Nairobi’s
+most blue-chip location, offers unobstructed, scenic
+views of the Ngong Hills and Nairobi CBD.
+The Reveal offers exquisite 1 Bedrooms,
+and Studios.</p>
         </div>
     </div>
   )
--- pages/residences.js
+++ pages/residences.js
@@ -1,18 +1,26 @@
+import Onebedroomcover from '@/components/Onebedroomcover'
+import Onebedroomfirst from '@/components/Onebedroomfirst'
 import Onebedroomslider from '@/components/Onebedroomslider'
 import ResidenceBanner from '@/components/ResidenceBanner'
 import ResidencesDisplay from '@/components/ResidencesDisplay'
 import ResidenceSecond from '@/components/ResidenceSecond'
 import ResidencesFirst from '@/components/ResidencesFirst'
+import Studiofirst from '@/components/Studiofirst'
+import Studiosecond from '@/components/Studiosecond'
 import React from 'react'
 
 function residences() {
   return (
     <div>
       <ResidenceBanner/>
-      <ResidencesFirst/>
+      {/* <ResidencesFirst/> */}
+      <Onebedroomfirst/>
+      <Studiofirst/>
       <Onebedroomslider/>
-      <ResidencesDisplay/>
-      <ResidenceSecond/>
+      {/* <ResidencesDisplay/> */}
+      {/* <ResidenceSecond/> */}
+      <Onebedroomcover/>
+      <Studiosecond/>
     </div>
   )
 }
