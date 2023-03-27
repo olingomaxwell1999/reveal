80e90b7d5ecd4a1fbe8126b7f4fbe36e800639af
olingomaxwell1999 <olingomaxwell1999@gmail.com>
2023-03-27 09:54:47 +0300

    minor

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import '../styles/globals.css'
 import Head from 'next/head'
 
 import Layout from '../components/Layout'
+import Script from 'next/script'
 
 function MyApp ({Component, pageProps}) {
 
@@ -12,6 +13,18 @@ function MyApp ({Component, pageProps}) {
         
       </Head>
       <Layout>
+        <Script strategy='afterInteractive' src='https://www.googletagmanager.com/gtag/js?id=G-WEDLPBY9W2'/>
+
+        <Script id='google-analytics' strategy='afterInteractive'>
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-WEDLPBY9W2', {
+              page_path: window.location.pathname
+            })
+          `}
+        </Script>
         <Component {...pageProps}/>
       </Layout>
     </>
