7f0821dafeff1bc182e0728dbb5dbc95ef44c615
olingomaxwell1999 <olingomaxwell1999@gmail.com>
2023-05-22 18:29:03 +0300

    metricool

--- pages/index.js
+++ pages/index.js
@@ -21,6 +21,13 @@ export default function Home() {
         <meta name="description" content="Come to The Reveal and we shall offer you the best housing you need while in Nairobi." />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              function loadScript(a){var b=document.getElementsByTagName("head")[0],c=document.createElement("script");c.type="text/javascript",c.src="https://tracker.metricool.com/resources/be.js",c.onreadystatechange=a,c.onload=a,b.appendChild(c)}loadScript(function(){beTracker.t({hash:"e8d4e123d329ca216a27723f23c6f43f"})});
+            `,
+          }}
+        ></script>
       </Head>
       <Homebanner/>
       <Revealarea/>
