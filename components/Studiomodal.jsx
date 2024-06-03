60e7bb69e6017ef68b576edafde6f8dc008c8d15
Olingo Maxwell <olingomaxwell1999@gmail.com>
2024-06-03 12:41:33 +0300

    studio plan

--- components/Studiomodal.jsx
+++ components/Studiomodal.jsx
@@ -1,5 +1,5 @@
 import Image from 'next/image'
-import Onebedroomplan from '../assets/FLOOR PLAN - STUDIOS.jpg'
+import Onebedroomplan from '../assets/studioplanmain.jpg'
 import React,{useEffect} from 'react'
 import AOS from 'aos'
 import 'aos/dist/aos.css'
